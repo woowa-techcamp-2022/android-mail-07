da45a9a6b585d00b5417d0ba7bad8d0a0fcf7dd5
eshc123 <eshc.joon@gmail.com>
2022-07-06 10:46:06 +0900

    init

---
+++ app/src/main/java/com/example/woowamailapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.woowamailapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.woowamailapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
