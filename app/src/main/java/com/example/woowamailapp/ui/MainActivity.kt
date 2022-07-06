5f29c6ccf3497bfb5b81d4f6be0203b7c3fae0d2
eshc123 <eshc.joon@gmail.com>
2022-07-07 00:03:32 +0900

    #4 feat : add drawer layout

--- app/src/main/java/com/example/woowamailapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/woowamailapp/ui/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.woowamailapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import com.example.woowamailapp.R
+import com.example.woowamailapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tbMain.setOnClickListener {
+            binding.dlMain.openDrawer(Gravity.LEFT)
+        }
     }
 }
